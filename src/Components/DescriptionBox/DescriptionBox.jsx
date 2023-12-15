5774ce404aea471e2cff68e6c40af045b01a3eef
yusennn <116973490+yusennn@users.noreply.github.com>
2023-12-15 14:25:22 +0600

    second commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox" >
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, est incidunt corporis debitis autem quisquam id, itaque nam illum eum, sapiente facere. Exercitationem minima quis error recusandae rerum porro beatae. Veritatis temporibus officiis corporis quibusdam tenetur fuga. Incidunt dolores quibusdam quidem molestias totam reprehenderit ratione, nobis quisquam laborum ad cum.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptate laudantium assumenda cum eum quae corporis suscipit nesciunt aliquam eius!</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
